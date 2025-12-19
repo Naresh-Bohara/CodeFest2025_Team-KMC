71aa54b6ea554126953aeea38b0abda2225a4973
hemraj11 <hemrajojha1122334455@gmail.com>
2025-12-19 16:21:25 +0545

    emergency-services module is completed

--- api/config/router.config.js
+++ api/config/router.config.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
 import authRouter from "../modules/auth/auth.router.js";
+import municipalityRouter from "../modules/municipalities/municipality.router.js";
+import emergencyRouter from "../modules/emergency-services/emergency-services.router.js";
 const router = Router();
 router.use("/auth", authRouter);
 
+router.use("/municipalities", municipalityRouter);
+router.use("/emergency-services", emergencyRouter);
 
 export default router;
